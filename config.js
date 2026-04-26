f1a8ce5fdd5c15ee055210e66fe6da797cc6e05a
dariuszflajszer-hub <dariuszflajszer@gmail.com>
2026-04-26 23:06:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const LIVE_CONFIG = {
   isLive: true,
-  liveUrl: "https://www.facebook.com/butiklilu/videos/1491006372748900/",
+  liveUrl: "https://www.facebook.com/butiklilu/videos/924676670398429",
   nextLiveText: "Czwartek 20:00"
 };
